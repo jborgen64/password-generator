dbcec5da597cccd523e994e1edc27c91e36e2979
son loku <jacobrborgen@gmail.com>
2019-12-19 11:14:12 -0600

    added string values for my variables

--- goodies.js
+++ goodies.js
@@ -1,28 +1,26 @@
-var upperChk
-var lowerChk
-var numberChk
-var specialChk
-var lengthInput
-var genBtn
+//assigning variables to buttons
 
-function randomUpperCase() {
-    var upletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return upletters[Math.floor(Math.random() * 26)]
+var charLengthSlide = document.getElementById("slidey-guy");
+var upperCaseChk = document.getElementById("upper-chk");
+var lowerCaseChk = document.getElementById("lower-chk");
+var numberChk = document.getElementById("number-chk");
+var supSpecChk = document.getElementById("sup-spec-chk");
+var passReturn = document.getElementById("password")
+
+//variables for value sets
+
+var upletters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowletters = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var symbol = "~!@#$%^&*()_+=-`';:,./?><";
+
+console.log(getPassword(6,upletters))
+
+function getPassword(l,valueset) {
+    pwd = "";
+    for (var i = 0; i<l; i++) {
+        pwd += valueset.charAt(Math.floor(Math.random() * valueset.length))
+    }
+    return pwd;
 }
-
-function randomLowerCase() {
-    var lowletters = "abcdefghijklmnopqrstuvwxyz";
-    return lowletters[Math.floor(Math.random() * 26)]
-}
-
-function randomNubmer() {
-    var number = "0123456789";
-    return upletters[Math.floor(Math.random() * 10)]
-}
-
-function randomSymbol() {
-    var symbol = "~!@#$%^&*()_+=-`';:,./?><";
-    return symbol[Math.floor(Math.random() * 25)]
-}
-
 
