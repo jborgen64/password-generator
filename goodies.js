43b383605794b1e1dea627812a2b9d55148fe044
son loku <jacobrborgen@gmail.com>
2019-12-19 11:23:02 -0600

    created functions that return random strings from existing variables that correspond to user criteria (uppercase, lowercase, number, symbol)

--- goodies.js
+++ goodies.js
@@ -5,22 +5,21 @@ var upperCaseChk = document.getElementById("upper-chk");
 var lowerCaseChk = document.getElementById("lower-chk");
 var numberChk = document.getElementById("number-chk");
 var supSpecChk = document.getElementById("sup-spec-chk");
-var passReturn = document.getElementById("password")
+var passReturn = document.getElementById("password");
 
 //variables for value sets
 
-var upletters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowletters = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
-var symbol = "~!@#$%^&*()_+=-`';:,./?><";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var number = "1234567890";
+var symbol = "!@#$%^&*()_+-=";
 
-console.log(getPassword(6,upletters))
 
-function getPassword(l,valueset) {
-    pwd = "";
-    for (var i = 0; i<l; i++) {
-        pwd += valueset.charAt(Math.floor(Math.random() * valueset.length))
-    }
-    return pwd;
+//create functions that are returning random outputs from variables
+
+function upperRandomReturn() {
+    return uppercase.charAt(Math.floor(Math.random() * uppercase.length))
 }
+
+console.log(upperRandomReturn())
 
